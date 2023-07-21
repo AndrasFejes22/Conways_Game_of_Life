1fadd95691096f9440a7c86dce9a0d78d8cf18ea
AndrasFejes22 <afejes22@gmail.com>
2023-07-21 22:09:59 +0200

    Calculating function added

--- src/conway/GameEngine.java
+++ src/conway/GameEngine.java
@@ -1,5 +1,8 @@
 package conway;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameEngine implements Runnable{
 
     private final Cell[][] cells;
@@ -13,6 +16,24 @@ public class GameEngine implements Runnable{
         while (true){
             System.out.println("Calculating");
             // counting
+            List<Cell> cellsToUpdate = new ArrayList<>();
+            for (int row = 0; row < cells.length; row++) {
+                for (int column = 0; column < cells[row].length; column++) {
+                    Cell cell = cells[row][column];
+                    int livingNeighbours = countLivingNeighbours(row, column);
+                    if(cell.isAlive()){
+                        if(livingNeighbours < 2 || livingNeighbours > 3){ //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                            // || Any live cell with more than three live neighbours dies, as if by overpopulation.
+                            cellsToUpdate.add(cell);
+                        }
+                    } else { //dead cell
+                        if(livingNeighbours == 3){ // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                            cellsToUpdate.add(cell);
+                        }
+                    }
+
+                }
+            }
             // UI update
             // sleep : 0.5 sec
             try {
@@ -21,5 +42,9 @@ public class GameEngine implements Runnable{
                 return;
             }
         }
+    }
+
+    private int countLivingNeighbours(int row, int column) {
+        return 0;
     }
 }
